7dbad02a9db37e3909c1fb6fb7f887dad00c3b4a
Octavio Navarro <octavio.navarro@tec.mx>
2022-10-21 18:20:47 -0500

    Added missing script for the isNight condition

---
+++ Shaders_ML/Assets/Code/Scripts/DayNightCycle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public event System.EventHandler OnChanged;
+
+    public float dayDuration = 10.0f;
+
+    public bool IsNight { get; private set; }
+
+    public Color nightColor = Color.white * 0.1f;
+
+    private Color dayColor;
+
+    private Light lightComponent;
+
+    void Start()
+    {
+        lightComponent = GetComponent<Light>();
+        dayColor = lightComponent.color;
+    }
+
+    void Update() {
+        float lightIntensity = 0.5f +
+                      Mathf.Sin(Time.time * 2.0f * Mathf.PI / dayDuration) / 2.0f;
+
+        bool shouldBeNight = lightIntensity < 0.3f;
+        if (IsNight != shouldBeNight) {
+            IsNight = shouldBeNight;
+            OnChanged?.Invoke(this, System.EventArgs.Empty); // Invoke event handler (if set).
+        }
+
+        lightComponent.color = Color.Lerp(nightColor, dayColor, lightIntensity);
+    }
+
+} 
